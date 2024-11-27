1e3e8cf363a4b31fcdfbc5f61f4e5b176e575f73
brunasso <53584556+brunasso@users.noreply.github.com>
2024-11-27 01:41:46 +0000

    Finalizando

--- src/models.py
+++ src/models.py
@@ -1,64 +1,71 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-from enum import Enum as PyEnum
+from sqlalchemy import DateTime
+
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(250), nullable=False)
+
+
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    subscribe_date = Column(DateTime)
+
+class Address(Base):
+    __tablename__ = 'address'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def to_dict(self):
+        return {}
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer)
+    weather = Column(String(50), nullable=True)
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    age = Column(Integer)
+    zodiac = Column(String(100))
+    #Poniendo los nombres entre comillas, puedo obviar el orden, en la relación.
     
+class Favorite(Base):
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    userName = Column(String(250), nullable=False)
-    firstName = Column(String(250), nullable=False)
-    lastName = Column(String(250), nullable=False)
-    email = Column(String(250), unique=True)
-
-class Follower(Base):
-    __tablename__ = 'follower'
     
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))
+    people_id = Column(Integer, ForeignKey('people.id'))
+    people = relationship(People)
+    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
 
 
-class Post(Base):
-    __tablename__ = 'post'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String(250), ForeignKey('user.id'))
-    author = relationship(User)
-
-class Comment(Base):
-    __tablename__ = 'comment'
-
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(800))
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    author = relationship(User)
-
-class MediaType(PyEnum):
-    REEL = "reel"
-    PICTURE = "picture"
-    STORY = "story"
-
-class Media(Base):
-    __tablename__ = 'media'
-
-    id = Column(Integer, primary_key=True)
-    type = Column(Enum(MediaType))
-    url = Column(String(250))
-    post_id = Column(Integer, ForeignKey('post.id'))
 
 
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
